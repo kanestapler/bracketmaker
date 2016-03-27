867c24027ec18c067ac7dfa94da2f3edbaad48c3
stapk007 <kane.stapler@disney.com>
2016-03-27 16:00:55 -0400

    Adding first typescript and old bracket data

---
+++ src/bracketMaker_new.ts
@@ -0,0 +1,14 @@
+class Team {
+    seed: number;
+    name: string;
+    constructor (iSeed: number, iName: string) {
+        this.seed = iSeed;
+        this.name = iName;
+    }
+}
+
+
+
+function getWinner(teamOne, teamTwo, round) {
+    return teamOne;
+}
