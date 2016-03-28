9a2061b3ffafed8197ea1ecbd40282a6d687ed09
stapk007 <kane.stapler@disney.com>
2016-03-27 20:56:10 -0400

    adding bracket checking

--- src/bracketMaker_new.ts
+++ src/bracketMaker_new.ts
@@ -19,6 +19,15 @@ class Score {
     eliteEightWon: number;
     finalFourWon: number;
     championshipWon: boolean;
+    constructor(iGamesWon: number, iFirstRoundWon: number, iSecondRoundWon: number, iSweetSixteen: number, iEliteEight: number, iFinalFour: number, ichampionshipWon: boolean) {
+        this.gamesWon = iGamesWon;
+        this.firstRoundWon = iFirstRoundWon;
+        this.secondRoundWon = iSecondRoundWon;
+        this.sweetSixteenWon = iSweetSixteen;
+        this.eliteEightWon = iEliteEight;
+        this.finalFourWon = iFinalFour;
+        this.championshipWon = ichampionshipWon;
+    }
 }
 
 class Bracket {
@@ -73,8 +82,42 @@ class Bracket {
     }
 }
 
-function checkBracketWithCorrectOne(bracket: Bracket, correctBracket: Bracket) {
-    let returnScore = new Score();
+function checkFullBracketWithCorrectOne(bracket: Bracket, correctBracket: Bracket) {
+    let returnScore = new Score(0,0,0,0,0,0,false);
+    for (let i = 0; i < 8; i++) {//Check First Round
+        for (let j = 0; j < 4; j++) {
+            if (bracket.regions[j].winners.firstRound[i] === correctBracket.regions[j].winners.firstRound[i]) {
+                returnScore.firstRoundWon = returnScore.firstRoundWon + 1;
+            }
+        }
+    }
+    for (let i = 0; i < 4; i++) {//Check Second Round
+        for (let j = 0; j < 4; j++) {
+            if (bracket.regions[j].winners.secondRound[i] === correctBracket.regions[j].winners.secondRound[i]) {
+                returnScore.secondRoundWon = returnScore.secondRoundWon + 1;
+            }
+        }
+    }
+    for (let i = 0; i < 2; i++) {//Check Sweet Sixteen
+        for (let j = 0; j < 4; j++) {
+            if (bracket.regions[j].winners.thirdRound[i] === correctBracket.regions[j].winners.thirdRound[i]) {
+                returnScore.sweetSixteenWon = returnScore.sweetSixteenWon + 1;
+            }
+        }
+    }
+    for (let i = 0; i < 4; i++) {//Check Elite Eight
+        if (bracket.regions[i].winners.fourthRound === correctBracket.regions[i].winners.fourthRound) {
+            returnScore.eliteEightWon = returnScore.eliteEightWon + 1;
+        }
+    }
+    for (let i = 0; i < 2; i++) {//Check Final Four
+        if (bracket.FF.winners.finalFour[i] === bracket.FF.winners.finalFour[i]) {
+            returnScore.finalFourWon = returnScore.finalFourWon + 1;
+        }
+    }
+    if (bracket.FF.winners.championship === bracket.FF.winners.championship) {
+        returnScore.championshipWon = true;
+    }
     return returnScore;
 }
 
@@ -87,6 +130,7 @@ function fillOutBlankBracket(iBracket: Bracket) {
             returnBracket.regions[j].winners.firstRound[i] = getWinner(returnBracket.regions[j].teams[i], returnBracket.regions[j].teams[i+1], 1).seed;
         }
     }
+    //Do Final Four and Championship
     return returnBracket;
 }
 
@@ -96,6 +140,7 @@ function jsonBracketToBracketClass(iBracket) {
     for (let i = 0; i < 4; i++) {
         returnBracket.regions[i].winners = iBracket.regions[i].winners;
     }
+    //Do Final Four and Championship
     return returnBracket;
 }
 
